2b0942f2ea9750ea6e6e31989dd0d996ab40617d
nɒmlǝbiƎ noɿA <aron@eideldev.com>
2021-08-24 16:49:36 -0700

    add POC Lambda
    to be extended post-testing

---
+++ src/lambda_function.py
@@ -0,0 +1,52 @@
+import boto3
+import json
+import uuid
+import datetime
+
+securityhub = boto3.client('securityhub')
+
+def lambda_handler(event, context):
+    all_findings = []
+    uid = event['data']['activity']['id']
+    fid = "us-east-1/021740258839/" + str(uid)
+    time = datetime.datetime.utcnow().isoformat("T") + "Z"
+    data = event['data']
+    reportAttributes = event['data']['report']['attributes']
+    severityRating = event['data']['report']['relationships']['severity']['data']['attributes']['rating'].upper()
+    # severityScore = str(event['data']['report']['relationships']['severity']['data']['attributes']['score'])
+
+    finding = {
+        "SchemaVersion": "2018-10-08",
+        "RecordState": "ACTIVE",
+        "ProductArn": "arn:aws:securityhub:us-east-1::product/hackerone/vulnerability-intelligence",
+        "ProductFields": {
+            "ProviderName": "HackerOne"  
+        },
+        "Description": reportAttributes['title'],
+        "GeneratorId": "acme-vuln-9ab348",
+        "AwsAccountId": "{---}",
+        "Id": fid,
+        "Types": [
+            "Software and Configuration Checks/Vulnerabilities/CVE"
+        ],
+        "CreatedAt": time,
+        "UpdatedAt": time,
+        "FirstObservedAt": time,
+        "Resources": [{
+            "Type": "AwsLambdaFunction",
+            "Id": "arn:aws:lambda:us-east-1:{---}:function:hackerone-receiver"
+        }],
+        "Severity": {
+            "Label": severityRating,
+            "Original": "5"
+        },
+        "Title": "HackerOne: " + reportAttributes['title']
+    }
+
+    all_findings.append(finding)
+
+    securityhub_cli = boto3.client('securityhub', region_name="us-east-1")
+
+    securityhub_cli.batch_import_findings(
+        Findings=all_findings
+    )
