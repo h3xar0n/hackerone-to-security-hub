073968d604df2c81143c5ea522d620f4e32cb1e3
noɿA <aron@eideldev.com>
2021-09-08 11:09:11 -0400

    add a new line between steps

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 echo "Enter the name of an S3 bucket you own to host the configuration:"
 read S3BUCKET
 aws cloudformation package --template-file template.yaml --output-template-file packaged-template.yaml --s3-bucket $S3BUCKET
-echo "Template file packaged in $S3BUCKET. Enter a name for your stack:"
+echo "Template file packaged in $S3BUCKET."
+echo "Enter a name for your stack:"
 read STACKNAME
 aws cloudformation deploy --template-file packaged-template.yaml --stack-name $STACKNAME --capabilities CAPABILITY_IAM
 echo "Target the HackerOne Webhook at the following URL:"
